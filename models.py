2b985d5b5d85a3661984be8465e18f19aaa7a7f3
Dylan Nguyen <dylannguyen@Dylans-MacBook-Pro-3.local>
2025-03-09 18:37:38 -0700

    Added Product and CartItem models

--- models.py
+++ models.py
@@ -21,3 +21,26 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
+    
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.string(200), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    image_url = db.Column(db.String(255), nullable=True) # Temporary; replace with actual image later
+
+    def __repr__(self):
+        return f"Product('{self.product_name}', '${self.price}')"
+    
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False, default=1)
+
+    user = db.relationship('User', backref=db.backref('cart_items', lazy=True))
+    product = db.relationship('Product', backref=db.backref('cart_items', lazy=True))
+
+    def __repr__(self):
+        return f"CartItem('{self.product.product_name}', '{self.quantity}')"
+    
