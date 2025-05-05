ba6c5d91499d072c85689257b1734e9aa6c3347e
DYLAB2331 <dylannguyen2331@gmail.com>
2025-05-05 01:18:59 -0700

    Added server name

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SERVER_NAME'] = 'bytemarket.duckdns.org'
 
 app.wsgi_app = ProxyFix(
     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
